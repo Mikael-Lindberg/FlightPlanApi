0ab0c24c9b9ce4f078cbbafd157f1e78357f7afb
mikael <mikael.lindberg93@gmail.com>
2024-02-25 17:09:37 +0100

    Added interface for database adapting

---
+++ Data/IDatabaseAdapter.cs
@@ -0,0 +1,13 @@
+﻿using FlightPlanApi.Models;
+
+namespace FlightPlanApi.Data
+{
+    public interface IDatabaseAdapter
+    {
+        Task<List<FlightPlan>> GetAllFightPlans();
+        Task<FlightPlan> GetFlightPlanById(string flightPlanId);
+        Task<bool> FileFlightPlan(FlightPlan flightPlan);
+        Task<bool> UpdateFlightPlan(string flightPlanId, FlightPlan flightPlan);
+        Task<bool> DeleteFlightPlanById(string flightPlanId);
+    }
+}
