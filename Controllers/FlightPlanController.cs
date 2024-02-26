8fb912c689f3758723b776459cee3be38ed5c0f2
mikael <mikael.lindberg93@gmail.com>
2024-02-26 07:29:00 +0100

    refactored data layer

--- Controllers/FlightPlanController.cs
+++ Controllers/FlightPlanController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlightPlanApi.Data;
+using FlightPlanApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightPlanApi.Controllers
@@ -7,5 +9,51 @@ namespace FlightPlanApi.Controllers
     [ApiController]
     public class FlightPlanController : ControllerBase
     {
+        private IDatabaseAdapter _database;
+
+        public FlightPlanController(IDatabaseAdapter database)
+        {
+            _database = database;
+        }
+
+        public async Task<IActionResult> FlightPlanList()
+        {
+
+        }
+
+        public async Task<IActionResult> GetFlightPlanById(string flightPlanId)
+        {
+
+        }
+
+        public async Task<IActionResult> FileFlightPlan(FlightPlan flightPlan)
+        {
+
+        }
+
+        public async Task<IActionResult> UpdateFlightPlan(FlightPlan flightPlan)
+        {
+
+        }
+
+        public async Task<IActionResult> DeleteFlightPlan(string flightPlanId)
+        {
+
+        }
+
+        public async Task<IActionResult> GetFlightPlanDepartureAirport(string flightPlanId)
+        {
+
+        }
+
+        public async Task<IActionResult> GetFlightPlanRoute(string flightPlanId)
+        {
+
+        }
+
+        public async Task<IActionResult> GetFlightPlanTimeEnroute(string flightPlanId)
+        {
+
+        }
     }
 }
--- Data/MongoDbDatabase.cs
+++ Data/MongoDbDatabase.cs
@@ -73,7 +73,7 @@ namespace FlightPlanApi.Data
             return flightPlan;
         }
 
-        public async Task<bool> FileFlightPlan(FlightPlan flightPlan)
+        public async Task<TransactionResult> FileFlightPlan(FlightPlan flightPlan)
         {
             var collection = GetCollection("pluralsight", "flight_plans");
 
@@ -99,16 +99,20 @@ namespace FlightPlanApi.Data
             try
             {
                 await collection.InsertOneAsync(document);
+                if (document["_id"].IsObjectId)
+                {
+                    return TransactionResult.Success;
+                }
+
+                return TransactionResult.BadRequest;
             }
             catch
             {
-                return false;
+                return TransactionResult.ServerError;
             }
-
-            return true;
         }
 
-        public async Task<bool> UpdateFlightPlan(string flightPlanId, FlightPlan flightPlan)
+        public async Task<TransactionResult> UpdateFlightPlan(string flightPlanId, FlightPlan flightPlan)
         {
             var collection = GetCollection("pluralsight", "flight_plans");
             var filter = Builders<BsonDocument>.Filter.Eq("flight_plan_id", flightPlanId);
@@ -129,7 +133,17 @@ namespace FlightPlanApi.Data
                 .Set("numberOnBoard", flightPlan.NumberOnBoard);
             var result = await collection.UpdateOneAsync(filter, update);
 
-            return result.ModifiedCount > 0;
+            if (result.MatchedCount == 0)
+            {
+                return TransactionResult.NotFound;
+            }
+
+            if (result.ModifiedCount > 0)
+            {
+                return TransactionResult.Success;
+            }
+
+            return TransactionResult.ServerError;
         }
 
         public async Task<bool> DeleteFlightPlanById(string flightPlanId)
